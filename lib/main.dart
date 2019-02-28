ffc7e89f28a091bd3eafcea9f695f421523f33b1
liujinyang <liujinyang@hachismart.com>
2019-02-28 14:48:11 +0800

    使用外部包（package）

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final wordPair=new WordPair.random();
     return new MaterialApp(
       title: 'LJY Flutter Demo',
       debugShowCheckedModeBanner: false,//设为false去除app右上角的debug标签
       home: new Scaffold(
         appBar: new AppBar(
-          title: new Text('Welcome to Flutter'),
+          title: new Text('Welcome to Flutter LJY'),
         ),
         body: new Center(
-          child: new Text('Hello World'),
+          child: new Text(wordPair.asPascalCase),
+//          child: new Text('Hello World'),
         ),
       ),
     );
